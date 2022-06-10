d70fd637e6cff4ebd9c7cd65f2b9fed35aa3846e
Zainal Azman Shaari <zainal.azman.shaari@quest.com>
2022-06-11 01:16:04 +0800

    first cut

--- builduserfile.py
+++ builduserfile.py
@@ -1,1 +1,12 @@
+import csv
 
+f1 = open('test.csv')
+f2 = open('malaysia2.csv','w',newline="") #remove blank line in between output
+csv_rd = csv.reader(f1)
+csv_wr = csv.writer(f2, delimiter=',', quotechar='"',quoting=csv.QUOTE_ALL) #use ',' as delimeter, '"' as quote and quote all items
+for row in csv_rd:
+    if ((row[6]) == "Malaysia"):
+        csv_wr.writerow(row)
+
+f1.close()
+f2.close()
